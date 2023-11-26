45643678917a1b8ba2cf50d4dc9b6dc9e80ecc22
Abdullah149081 <masudmd425@gmail.com>
2023-11-26 21:44:58 +0600

    user interface create

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,28 @@
+export type IFullName = {
+    firstName: string;
+    lastName: string;
+};
+
+export type IAddress = {
+    street: string;
+    city: string;
+    country: string;
+};
+export type IOrders = {
+    productName: string;
+    price: number;
+    quantity: number;
+};
+
+export interface IUser {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: IFullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: IAddress;
+    orders?: IOrders[];
+}
